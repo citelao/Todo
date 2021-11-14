1efdf6a9ac4b9863c55b48c7738f0c2351b41c6f
Ben Stolovitz <ben@stolovitz.com>
2021-11-14 00:45:32 -0800

    more nesting

--- src/model/TodoApp.tsx
+++ src/model/TodoApp.tsx
@@ -12,7 +12,11 @@ export default class TodoApp {
     {
         const todos: ITodo[] = [
             { id: 0, title: "Pick up eggs", dueDate: new Date(), children: [
-                { id: 1000, title: "Grocery shop", },
+                { id: 1000, title: "Grocery shop", children: [
+                    { id: 100001, title: "Grocery shop a", },
+                    { id: 100002, title: "Grocery shop b", },
+                    { id: 100003, title: "Grocery shop c", },
+                ]},
                 { id: 1001, title: "Grocery shop 2", },
                 { id: 1002, title: "Grocery shop 3", },
                 { id: 1003, title: "Grocery shop 4", },
