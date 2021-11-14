9c0f74e242cb914f8b9d7a5e77812ec6a24457a4
Ben Stolovitz <ben@stolovitz.com>
2021-11-13 23:06:03 -0800

    a todo app

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const todos = [
+  { id: 0, title: "Pick up eggs", },
+  { id: 1, title: "Grocery shop", },
+  { id: 2, title: "Dance", },
+  { id: 3, title: "Fly", },
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map((t) => {
+          return <li key={t.id}>{t.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
