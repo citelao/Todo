147b228e63d64d76414e805f30d8bd77b51ab882
Ben Stolovitz <ben@stolovitz.com>
2021-11-13 23:45:21 -0800

    navigation

---
+++ src/utilities/KeyCodes.ts
@@ -0,0 +1,9 @@
+const KeyCodes = {
+    ArrowDown: "ArrowDown",
+    ArrowUp: "ArrowUp",
+    ArrowLeft: "ArrowLeft",
+    ArrowRight: "ArrowRight",
+    Enter: "Enter",
+};
+
+export default KeyCodes;
--- src/view/TreeGrid.tsx
+++ src/view/TreeGrid.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
+import KeyCodes from "../utilities/KeyCodes";
 
 interface IItem {
 
@@ -9,10 +10,18 @@ interface ITreeViewProperties {
 }
 
 interface ITreeViewState {
-
+    selectedIndex: number;
 }
 
 export default class TreeGrid extends React.Component<ITreeViewProperties, ITreeViewState> {
+    constructor(props: ITreeViewProperties) {
+        super(props);
+        this.state = {
+            selectedIndex: 0,
+        };
+    }
+
+
     public render() {
         return <table role="treegrid">
             <thead>
@@ -27,7 +36,7 @@ export default class TreeGrid extends React.Component<ITreeViewProperties, ITree
     private renderRow = (item: IItem, index: number) =>
     {
         const isFocusedOnRows = true; // TODO
-        const isSelectedItem = index == 0; // TODO
+        const isSelectedItem = index == this.state.selectedIndex; // TODO
         const hasChildren = false;
         const isExpanded = false; // TODO
         const level = 1; // TODO
@@ -37,7 +46,8 @@ export default class TreeGrid extends React.Component<ITreeViewProperties, ITree
             aria-expanded={(hasChildren) ? isFocusedOnRows && isExpanded : undefined}
             aria-level={level}
             aria-posinset={index + 1}
-            aria-setsize={this.props.items!.length}>
+            aria-setsize={this.props.items!.length}
+            onKeyUp={this.handleRowKeyUp}>
             <td role="gridcell">
                 Todo
             </td>
@@ -45,5 +55,34 @@ export default class TreeGrid extends React.Component<ITreeViewProperties, ITree
                 Todo
             </td>
         </tr>;
+    }
+
+    private handleRowKeyUp = (e: KeyboardEvent<HTMLElement>) => {
+        switch (e.key) {
+            case KeyCodes.ArrowUp:
+                {
+                    const item = e.target as HTMLElement;
+                    const newItem = item.parentElement?.children[this.state.selectedIndex - 1] as HTMLElement;
+                    newItem.focus();
+                    this.setState({
+                        selectedIndex: this.state.selectedIndex - 1
+                    });
+                }
+                break;
+
+            case KeyCodes.ArrowDown:
+                {
+                    const item = e.target as HTMLElement;
+                    const newItem = item.parentElement?.children[this.state.selectedIndex + 1] as HTMLElement;
+                    newItem.focus();
+                    this.setState({
+                        selectedIndex: this.state.selectedIndex + 1
+                    });
+                }
+                break;
+
+            default:
+                console.log(e.key);
+        }
     }
 }
